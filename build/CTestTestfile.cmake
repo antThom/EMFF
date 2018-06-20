9bc0925c0657821bad3e5cb6235a08cda40da3bc
antThom <anthonyat@berkeley.edu>
2018-06-20 14:21:49 -0700

    Built a ROS environment

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/anthony/EMFF/src
+# Build directory: /home/anthony/EMFF/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
