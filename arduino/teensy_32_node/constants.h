cfc6436a4a289dc25a74f3084069fdbc13b2c69e
Anthony Thompson <anthonyat@berkeley.edu>
2018-08-06 20:01:15 -0700

    added H-bridge logic and a global duty cycle not tested yet

--- arduino/teensy_32_node/constants.h
+++ arduino/teensy_32_node/constants.h
@@ -15,8 +15,11 @@
     float ohm = 0.5;              //resistance
     float Vbat = 11.1;            //Voltage of the Battery
     float N = 25.0;               //# of loops
-    float R = 0.3;                //Radius of coil meters
+    float R = 0.156775;           //Radius of coil meters
     float A = pi*R*R;             //Area of coil meters^2
+    int timeON = 30;            //Amount of time the coils will be on (seconds)
+    float GDC = 0.50;             //Global duty cycle of the coils (coil ON / coil off)
+    float timeOFF = timeON/GDC;   //Amount of time the coils will be off (seconds)
     
     //Pin Assignment
     const int curSens1 = 0;        //Analog pin 0
@@ -24,14 +27,14 @@
     const int imu_LED = 12;        //Digital pin 12
     const int mag_LED = 11;        //Digital pin 11
     const int LED = 10;            //Good status LED Digital pin 10
-    const int coil_11_dir = 6;      //Direction of coil_1
-    const int coil_12_dir = 7;      //Direction of coil_2
-    const int coil_21_dir = 8;      //Direction of coil_1
-    const int coil_22_dir = 9;      //Direction of coil_2
-    const int coil_11_pwm = 23;     //DutyCycle of coil_1
-    const int coil_12_pwm = 22;     //DutyCycle of coil_2
-    const int coil_21_pwm = 21;     //DutyCycle of coil_1
-    const int coil_22_pwm = 20;     //DutyCycle of coil_2
+    const int coil_11_dir = 23;      //Direction of coil_1
+    const int coil_12_dir = 20;      //Direction of coil_1
+    const int coil_21_dir = 3;      //Direction of coil_1
+    const int coil_22_dir = 6;      //Direction of coil_2
+    const int coil_11_pwm = 22;     //DutyCycle of coil_1
+    const int coil_12_pwm = 21;     //DutyCycle of coil_2
+    const int coil_21_pwm = 4;     //DutyCycle of coil_1
+    const int coil_22_pwm = 5 ;     //DutyCycle of coil_2
 
     //System Variables
     float CS_Vout;
@@ -41,7 +44,7 @@
     float roll;
     float DC_x;
     float DC_y;
-    float time_0, time_1, dt;
+    float time_0, time_1, dt, coilTime_0, coilTime_1, coil_dt;
     LIS3MDL mag;
     LSM6 acc_gyr;
 
--- arduino/teensy_32_node/teensy_32_node.ino
+++ arduino/teensy_32_node/teensy_32_node.ino
@@ -1,136 +1,152 @@
 /*
- * This code should be uploaded onto a Teensy 3.2
- * This code should recieve incoming messages from
- * the odroid which got the message from the ground 
- * station. The objective of this code is to:
- *  1. Collect IMU data
- *  2. Collect current data
- *  3. Collect temperature data
- *  4, Operate the H-Bridge to power the coil
- *  5. Communicate with the Odroid via. ROS
- *  
- */
+   This code should be uploaded onto a Teensy 3.2
+   This code should recieve incoming messages from
+   the odroid which got the message from the ground
+   station. The objective of this code is to:
+    1. Collect IMU data
+    2. Collect current data
+    3. Collect temperature data
+    4, Operate the H-Bridge to power the coil
+    5. Communicate with the Odroid via. ROS
 
- #include <ros.h>
- #include <geometry_msgs/Twist.h> //Add the message types
- #include <geometry_msgs/Pose.h>
- #include <geometry_msgs/Vector3.h>  
- #include <std_msgs/Float32.h>
- #include <sensor_msgs/Imu.h>
- #include <LIS3MDL.h>
- #include <LSM6.h>
- #include <Wire.h>
- #include <math.h>
- 
- #include "constants.h"
+*/
+
+#include <ros.h>
+#include <geometry_msgs/Twist.h> //Add the message types
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Vector3.h>
+#include <std_msgs/Float32.h>
+#include <sensor_msgs/Imu.h>
+#include <LIS3MDL.h>
+#include <LSM6.h>
+#include <Wire.h>
+#include <math.h>
+
+#include "constants.h"
 
 
 
 //Callback Functions for COMMANDS coming from the groundStation
 /*
- void velocityCallback( const geometry_msgs::Twist& velocityCommand)
- {
-     digitalWrite(13, HIGH-digitalRead(13));  //Blink on board LED when a message is recieved 
- }
+  void velocityCallback( const geometry_msgs::Twist& velocityCommand)
+  {
+     digitalWrite(13, HIGH-digitalRead(13));  //Blink on board LED when a message is recieved
+  }
 */
 
- void positionCallback( const geometry_msgs::Pose& positionCommand)
- {
-   /* The incoming position command will be sent from MOCAP and should only be used as an 
-    * initial condition for intergration. For now I think we are mostly concerned with the
-    * attitude and attitude rates
-    *  1. Get orientation data
-    *  2. Convert to euler angles
-    *  3. Save the first set of data and save it as an Initial Condition
-    *  4. Ignore the rest of incoming data
-    */
+void positionCallback( const geometry_msgs::Pose& positionCommand)
+{
+  /* The incoming position command will be sent from MOCAP and should only be used as an
+     initial condition for intergration. For now I think we are mostly concerned with the
+     attitude and attitude rates
+      1. Get orientation data
+      2. Convert to euler angles
+      3. Save the first set of data and save it as an Initial Condition
+      4. Ignore the rest of incoming data
+  */
 
-    if (dataSet==1)
-    {
-      float x = positionCommand.orientation.x;
-      float y = positionCommand.orientation.y;
-      float z = positionCommand.orientation.z;
-      float w = positionCommand.orientation.w;
+  if (dataSet == 1)
+  {
+    float x = positionCommand.orientation.x;
+    float y = positionCommand.orientation.y;
+    float z = positionCommand.orientation.z;
+    float w = positionCommand.orientation.w;
 
-      //Convert Quaternion to euler angles and these will be the initail conditions
-      yaw = atan2(2*(x*y+w*z),(w*w-z*z-y*y+x*x)); // This is the most important one because our system shuldn't be changing it pitch or roll at all 
-      pitch = asin(-2*(x*z-y*w));
-      roll = atan2(2*(z*y+x*w),(w*w+z*z-y*y-x*x));
-      dataSet++;
-    }
-    digitalWrite(LED, HIGH-digitalRead(LED));   // blink the led
- }
-
-
- void magnetCallback( const geometry_msgs::Vector3& magnetCommand)
- {
-    /* The magnet commands will come in from MATLAB and will actually be a current through the coils command.
-     * The direction of the field is pre-determined and the incoming commands will correspond to each coil. 
-     * For example, the magnetCommand.X is for coil #1 and magnetCommand.Y is for coil #2.
-     * 
-     *  When we get magnet commands we need to convert that to a PWM dutycyle and calculate the magnetic
-     * dipole moment (mu) and magnetic field (B). To do the conversions and calculations we need to first 
-     * state some system parameters.
-     *  A = Area of Coil
-     *  N = Number of loops in the coil
-     *  L = Length of Coil (2*pi*R)
-     *  R = Radius of the Coil
-     *  Vbat = Voltage of the battery
-     *  ohm = Resistance of the coil
-     * 
-     * Operation Sequence
-     *  1. Get command
-     *  2. Convert command into a dutycycle
-     *  3. Calculate mu and B
-     *  4. Send dutycycle and direction to the H-Bridge controller.
-     */
-
-     // Convert the currents into DutyCycles (Resistance increases as the coils heat up so this is not the best approach)
-     float DC_x = (magnetCommand.X * ohm) / Vbat; //Domain = [0,1]
-     float DC_y = (magnetCommand.Y * ohm) / Vbat; //Domain = [0,1]
-
-     // Map these into a pwm signal [0,255]
-     DC_x = map(DC_x,0,1,0,255); //new Domain = [0,255] for pwm
-     DC_y = map(DC_y,0,1,0,255)
+    //Convert Quaternion to euler angles and these will be the initail conditions
+    yaw = atan2(2 * (x * y + w * z), (w * w - z * z - y * y + x * x)); // This is the most important one because our system shuldn't be changing it pitch or roll at all
+    pitch = asin(-2 * (x * z - y * w));
+    roll = atan2(2 * (z * y + x * w), (w * w + z * z - y * y - x * x));
+    dataSet++;
+  }
+  digitalWrite(LED, HIGH - digitalRead(LED)); // blink the led
+}
 
 
-/* THIS PORTION WILL NOT WORK MUST FIX
- *  
-     //Determine DIR Pin Values and apply
-     if (magnetCommand.X < 0)
-        digitalWrite(coil_11_dir,LOW); 
-        digitalWrite(coil_12_dir,HIGH);
-        analogWrite(coil_11_pwm,0); 
-        analogWrite(coil_12_pwm,0); 
-     else if (magnetCommand.X==0)
-        digitalWrite(coil_11_dir, HIGH);
-        digitalWrite(coil_12_dir, LOW);
-        analogWrite(coil_11_pwm,255); 
-        analogWrite(coil_12_pwm,255);
-     else
-        digitalWrite(coil_11_dir,HIGH);
-        digitalWrite(coil_12_dir,LOW); 
+void magnetCallback( const geometry_msgs::Vector3& magnetCommand)
+{
+  /* The magnet commands will come in from MATLAB and will actually be a current through the coils command.
+     The direction of the field is pre-determined and the incoming commands will correspond to each coil.
+     For example, the magnetCommand.X is for coil #1 and magnetCommand.Y is for coil #2.
 
-     if (magnetCommand.Y < 0)
-        digitalWrite(coil_21_dir,LOW); 
-        digitalWrite(coil_22_dir,HIGH); 
-     else
-        digitalWrite(coil_21_dir,HIGH);
-        digitalWrite(coil_22_dir,LOW); 
+      When we get magnet commands we need to convert that to a PWM dutycyle and calculate the magnetic
+     dipole moment (mu) and magnetic field (B). To do the conversions and calculations we need to first
+     state some system parameters.
+      A = Area of Coil
+      N = Number of loops in the coil
+      L = Length of Coil (2*pi*R)
+      R = Radius of the Coil
+      Vbat = Voltage of the battery
+      ohm = Resistance of the coil
 
-     //Apply the DutyCyle to the H-Bridge Controller
-     analogWrite(coil_11_pwm,DC_x); // coil_11_pwm should be the same as coil_12_pwm
-     analogWrite(coil_12_pwm,DC_x);
-     analogWrite(coil_21_pwm,DC_y);
-     analogWrite(coil_22_pwm,DC_y);
-     */
- }
+     Operation Sequence
+      1. Get command
+      2. Convert command into a dutycycle
+      3. Calculate mu and B
+      4. Send dutycycle and direction to the H-Bridge controller.
+  */
+
+  // Convert the currents into DutyCycles (Resistance increases as the coils heat up so this is not the best approach)
+  float DC_x = (magnetCommand.x * ohm) / Vbat; //Domain = [0,1]
+  float DC_y = (magnetCommand.y * ohm) / Vbat; //Domain = [0,1]
+
+  // Map these into a pwm signal [0,255]
+  DC_x = map(DC_x, 0, 1, 0, 255); //new Domain = [0,255] for pwm
+  DC_y = map(DC_y, 0, 1, 0, 255);
+
+  //Determine DIR Pin Values and apply
+
+  //COIL #1
+  if (magnetCommand.x < 0)                 //REVERSE
+  {
+    digitalWrite(coil_11_dir, LOW);
+    digitalWrite(coil_12_dir, LOW);
+    analogWrite(coil_11_pwm, 0);
+    analogWrite(coil_12_pwm, DC_x);
+  }
+  else if (magnetCommand.x > 0)            //FORWARD
+  {
+    digitalWrite(coil_11_dir, HIGH);
+    digitalWrite(coil_12_dir, HIGH);
+    analogWrite(coil_11_pwm, DC_x);
+    analogWrite(coil_12_pwm, 0);
+  }
+  else                                     //BRAKE
+  {
+    digitalWrite(coil_11_dir, LOW);
+    digitalWrite(coil_12_dir, HIGH);
+    analogWrite(coil_11_pwm, 255);
+    analogWrite(coil_12_pwm, 0);
+  }
+
+  // COIL #2
+  if (magnetCommand.y < 0)                 //REVERSE
+  {
+    digitalWrite(coil_21_dir, LOW);
+    digitalWrite(coil_22_dir, HIGH);
+    analogWrite(coil_21_pwm, 0);
+    analogWrite(coil_22_pwm, DC_y);
+  }
+  else if (magnetCommand.y > 0)            //FORWARD
+  {
+    digitalWrite(coil_21_dir, HIGH);
+    digitalWrite(coil_22_dir, LOW);
+    analogWrite(coil_21_pwm, DC_y);
+    analogWrite(coil_22_pwm, 0);
+  }
+  else                                     //BRAKE
+  {
+    digitalWrite(coil_21_dir, LOW);
+    digitalWrite(coil_22_dir, HIGH);
+    analogWrite(coil_21_pwm, 255);
+    analogWrite(coil_22_pwm, 0);
+  }
+}
 
 ///////////////////////{ SUBSCRIBE }////////////////////////////////////////
 // Initialized the Subscriber (must know the msg type and the topic that it is subscribing to
- ros::Subscriber<geometry_msgs::Pose> pos("POSITION", &positionCallback ); //Topic == POSITION && Node == pos
- //  ros::Subscriber<geometry_msgs::Twist> velocity("VELOCITY", &velocityCallback ); //Topic == VELOCITY && Node == velocity
- ros::Subscriber<geometry_msgs::Vector3> magnet("MAGNET", &magnetCallback );  //Topic == MAGNET && Node == magnet
+ros::Subscriber<geometry_msgs::Pose> pos("POSITION", &positionCallback ); //Topic == POSITION && Node == pos
+//  ros::Subscriber<geometry_msgs::Twist> velocity("VELOCITY", &velocityCallback ); //Topic == VELOCITY && Node == velocity
+ros::Subscriber<geometry_msgs::Vector3> magnet("MAGNET", &magnetCallback );  //Topic == MAGNET && Node == magnet
 ///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////{ PUBLISH }////////////////////////////////////////
@@ -150,12 +166,12 @@ ros::Publisher imu("IMU", &IMU);
 //////////////////////////////////////////////////////////////////
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT); //This the onboard LED
-  pinMode(LED,OUTPUT);
-  pinMode(imu_LED,OUTPUT);
-  pinMode(mag_LED,OUTPUT);
-  
-  digitalWrite(13,HIGH); //Turn on initialization LED
+  pinMode(13, OUTPUT); //This the onboard LED
+  pinMode(LED, OUTPUT);
+  pinMode(imu_LED, OUTPUT);
+  pinMode(mag_LED, OUTPUT);
+
+  digitalWrite(13, HIGH); //Turn on initialization LED
   nh.initNode();
   nh.subscribe(pos);
   //  nh.subscribe(velocity);
@@ -168,109 +184,110 @@ void setup() {
 
   Wire.begin(); //Initialize I2C
 
-  // Make sure the sensors get initialized 
+  // Make sure the sensors get initialized
   if (!acc_gyr.init())
   {
     //Turn on imu_LED
-    digitalWrite(imu_LED,HIGH);
-    while(1);
+    digitalWrite(imu_LED, HIGH);
+    while (1);
   }
   acc_gyr.enableDefault();
   acc_gyr.writeReg(LSM6::CTRL1_XL, 0x3C); // 52 Hz, 8 g full scale
   acc_gyr.writeReg(LSM6::CTRL2_G, 0x4C); // 104 Hz, 2000 dps full scale
-  
+
   if (!mag.init())
   {
     //Turn on mag_LED
-    digitalWrite(mag_LED,HIGH);
-    while(1);
+    digitalWrite(mag_LED, HIGH);
+    while (1);
   }
   mag.enableDefault();
   delay(1000);
-  analogWrite(LED,127);//Turn on good status LED
-  digitalWrite(13,LOW); //Turn off initialization LED
-  time_0=millis(); //Initial Time
+  analogWrite(LED, 127); //Turn on good status LED
+  digitalWrite(13, LOW); //Turn off initialization LED
+  time_0 = millis(); //Initial Time
+  coilTime_0 = millis();//Initial Time for the coils
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
   //Read IMU Data
   mag.read();         //get data from the magnetometer
   acc_gyr.readAcc();  //get data from the accelerometer
-  
+
   acc_gyr.readGyro();     //get data from the accelerometer and gyro
-  time_1=millis();    //time of sensor reading
-  dt=time_1-time_0;   //Time difference
-  time_0=time_1;      //Set new initial time for next integration
-  
+  time_1 = millis();  //time of sensor reading
+  dt = time_1 - time_0; //Time difference
+  time_0 = time_1;    //Set new initial time for next integration
+
   //Assign values to the magnet
-  magnetApplied.x=mag.m.x / magGain; 
-  magnetApplied.y=mag.m.y / magGain; 
-  magnetApplied.z=mag.m.z / magGain;
+  magnetApplied.x = mag.m.x / magGain;
+  magnetApplied.y = mag.m.y / magGain;
+  magnetApplied.z = mag.m.z / magGain;
 
 
   IMU.header.frame_id = "IMU"; // Give the Sensor an ID
   IMU.header.stamp = nh.now(); // This is NEEDED TO GET A TIME STEP
 
   //Assign values to the accelerometer
-  IMU.linear_acceleration.x=acc_gyr.a.x * accGain/GRAVITY*gravity; 
-  IMU.linear_acceleration.y=acc_gyr.a.y * accGain/GRAVITY*gravity; 
-  IMU.linear_acceleration.z=acc_gyr.a.z * accGain/GRAVITY*gravity;  
+  IMU.linear_acceleration.x = acc_gyr.a.x * accGain / GRAVITY * gravity;
+  IMU.linear_acceleration.y = acc_gyr.a.y * accGain / GRAVITY * gravity;
+  IMU.linear_acceleration.z = acc_gyr.a.z * accGain / GRAVITY * gravity;
 
   //Assign values to the gyro
-  IMU.angular_velocity.x=acc_gyr.g.x / gyroGain; 
-  IMU.angular_velocity.y=acc_gyr.g.y / gyroGain; 
-  IMU.angular_velocity.z=acc_gyr.g.z / gyroGain;
+  IMU.angular_velocity.x = acc_gyr.g.x / gyroGain;
+  IMU.angular_velocity.y = acc_gyr.g.y / gyroGain;
+  IMU.angular_velocity.z = acc_gyr.g.z / gyroGain;
 
   //Assign values to the orientation
-  float yawRate=acc_gyr.g.z;
-  yaw=integrator(yawRate);  //Not being published as of yet
-  
-  IMU.orientation.x=0.0; //We're getting data from MOCAP so I ignored these values
-  IMU.orientation.y=0.0; 
-  IMU.orientation.z=0.0; 
-  IMU.orientation.w=0.0;
-  
+  float yawRate = acc_gyr.g.z;
+  yaw = integrator(yawRate); //Not being published as of yet
+
+  IMU.orientation.x = 0.0; //We're getting data from MOCAP so I ignored these values
+  IMU.orientation.y = 0.0;
+  IMU.orientation.z = 0.0;
+  IMU.orientation.w = 0.0;
+
 
   //Assign values to the Current Sensor
   current1.data = currentSensor(analogRead(curSens1));
   current2.data = currentSensor(analogRead(curSens2));
-  
-  // Once we have the current we can calculate the dipole moment (mu) and the Magnetic Field (B)
-  float mu_x = current1.data*N*A; 
-  float mu_y = current2.data*N*A; 
 
-  
+
   /*//////////////////////{ SAMPLE CODE, NEEDS TO BE DELETED } //////////////////////////////
-  current.data = 1.0;
-  magnetApplied.x=1.0; magnetApplied.y=2.0; magnetApplied.z=3.0;
-  IMU.orientation.x=1.0; IMU.orientation.y=1.0; IMU.orientation.z=1.0; IMU.orientation.w=1.0;
-  IMU.angular_velocity.x=0.0; IMU.angular_velocity.y=0.0; IMU.angular_velocity.z=0.0;
-  IMU.linear_acceleration.x=4.0; IMU.linear_acceleration.y=4.0; IMU.linear_acceleration.z=4.0;
+    current.data = 1.0;
+    magnetApplied.x=1.0; magnetApplied.y=2.0; magnetApplied.z=3.0;
+    IMU.orientation.x=1.0; IMU.orientation.y=1.0; IMU.orientation.z=1.0; IMU.orientation.w=1.0;
+    IMU.angular_velocity.x=0.0; IMU.angular_velocity.y=0.0; IMU.angular_velocity.z=0.0;
+    IMU.linear_acceleration.x=4.0; IMU.linear_acceleration.y=4.0; IMU.linear_acceleration.z=4.0;
   *//////////////////////////////////////////////////////////////////////////////////////////
 
-  
+
   // Publish the applied values to their topics
   magnet_applied.publish( &magnetApplied );
   current_applied_1.publish( &current1 );
   current_applied_2.publish( &current2 );
   imu.publish( &IMU );
-  
+
   nh.spinOnce();
   delay(10);
+  coilTime_1 = millis(); //end of every loop
+  coil_dt = coilTime_1 - coilTime_0; //Time used to determine whether we should turn of the coils.
+
+  coilActivate(coil_dt);
 }
 
 
 
 
 
-///////////////////////////////{ USER DEFINED FUNCTIONS }///////////////////////////////////////  
+///////////////////////////////{ USER DEFINED FUNCTIONS }///////////////////////////////////////
 
 float currentSensor(float CS_Vout)
 {
-  CS_Vout = map(CS_Vout,0,1023,0,3.3);
-  float c = (CS_Vout-0.5*VCC_Teensy)/(0.185);
+  CS_Vout = map(CS_Vout, 0, 1023, 0, 3.3);
+  float c = (CS_Vout - 0.5 * VCC_Teensy) / (0.185);
   //float c = 73.3*(CS_Vout/VCC_Teensy)-36.7;
   return c;
 }
@@ -278,8 +295,29 @@ float currentSensor(float CS_Vout)
 
 float integrator(float yawRate)
 {
-  yaw = yaw + yawRate*dt;
+  yaw = yaw + yawRate * dt;
   return yaw;
 }
 
+void coilActivate(float coil_dt)
+{
+  float dt = round(coil_dt)%timeON; //if a time of timeOn seconds has elapsed then this mod should give us a zero (PLEASE JUST MAKE THIS TRUE)
+  
+  if (coil_dt >= timeON && dt==0) //If this is true then we want to brake
+  {
+    //Coil #2
+    digitalWrite(coil_21_dir, LOW);
+    digitalWrite(coil_22_dir, HIGH);
+    analogWrite(coil_21_pwm, 255);
+    analogWrite(coil_22_pwm, 0);
+
+    //Coil #1
+    digitalWrite(coil_11_dir, LOW);
+    digitalWrite(coil_12_dir, HIGH);
+    analogWrite(coil_11_pwm, 255);
+    analogWrite(coil_12_pwm, 0);
+
+    delay(timeOFF);
+  } 
+}
 
